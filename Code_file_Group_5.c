0a72cfacc2eb38eb088a076c4b2c5f7d21aa0871
Komalpreet090303 <117517892+Komalpreet090303@users.noreply.github.com>
2022-11-07 13:02:50 +0530

    Update Code_file_Group_5.c
    addition of function to construct rope

--- Code_file_Group_5.c
+++ Code_file_Group_5.c
@@ -1,5 +1,46 @@
 #include<stdio.h>
 #include<stdlib.h>
+void construct_rope(struct rope **root, struct rope *par, char array[], int lft, int rht)
+{
+
+    struct rope *temp = (struct rope *)malloc(sizeof(struct rope));
+    temp->left = NULL;
+    temp->right = NULL;
+    temp->parent = par;
+    if (rht - lft > 4)
+    {
+        // printf("hell");
+        temp->para = NULL;
+        temp->alp = (rht - lft) / 2;
+        //temp->coumt=0;
+        (*root) = temp;
+        int n = (rht + lft) / 2;
+        construct_rope(&((*root)->left), *root, array, lft, n);
+        construct_rope(&((*root)->right), *root, array, n + 1, rht);
+    }
+    else
+    {
+        // printf("hello");
+        *root = temp;
+        temp->alp = rht - lft;
+        // if(rht<lft){
+        //     temp=NULL;
+        //     return;
+        // }
+        int j = 0;
+        char *ans = (char *)malloc(4 * sizeof(char));
+        temp->para = ans;
+        // for(int i=0;i<4;i++){
+        //     ans[i]=0;
+        // }
+        temp->coumt = rht - lft + 1;
+        for (int i = lft; i <= rht; i++)
+        {
+            ans[j] = array[i];
+            j++;
+        }
+    }
+}
 char chr_index(int index_of_element, struct rope *root)
 {
     struct rope *temp = root;
